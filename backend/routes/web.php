29f0aec8d7268959fd52c237ea89648a56dca740
Teuku Atha Athaya Nafi <teukuatha19@gmail.com>
2026-05-27 14:04:21 +0700

    test v1

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -30,6 +30,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return response()->json([
-        'app' => 'KampusMarket API'
+        'app' => 'KampusMarket API - test v1'
     ]);
 });
