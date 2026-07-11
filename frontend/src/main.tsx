7535fd37851140ac3e20bcaccb455a58869072b8
Abdurrahman Ichwan <abdurrahmanichwan5@gmail.com>
2026-07-11 12:48:54 +0700

    add: console clearer

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+if (import.meta.env.PROD) {
+  console.log = () => {};
+  console.info = () => {};
+  console.warn = () => {};
+  console.error = () => {};
+  console.debug = () => {};
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
